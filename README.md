e166923729bad5e218b04e49792774ea484dd71f
Nebrocebro <116791937+Nebrocebro@users.noreply.github.com>
2023-05-20 00:07:59 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
-Spelidéer: 
-* Minesweeper
-* Solitaire
-* Tycoon/AdVenture Communist like
-* Mattespel, typ 2048 m.m.
-* Ngn klassiker, ksk pong eller nåt arcade-spel?
+Funktionen changeEasy() ändrar svårighetsgraden till "Lätt" genom att uppdatera komponenterna och ändra stilen på svårighetsgradsknapparna.
 
-Funktioner som måste undersökas:
-* Matten och koden omkring att kunna flytta karaktären ifall spelet har en karaktär
-* Optimisera och reducera lag, tex. inte räkna allt hela tiden och stäng av vid vissa ställen,
-    Gör inte matten för en ruta förrän den är nära karaktären, exempelvis minorna i minesweeper
+Funktionen changeMedium() ändrar svårighetsgraden till "Medium" på samma sätt som changeEasy().
+
+Funktionen changeHard() ändrar svårighetsgraden till "Svår" på samma sätt som changeEasy().
+
+Funktionen diffChecker() kontrollerar vilken svårighetsgrad som är vald och startar spelet med den svårighetsgraden.
+
+Funktionen startGame() startar spelet genom att dölja startknappen, visa omstartsknappen och skapa spelplanen.
+
+Funktionen placeBombs() placeras ut bomber slumpmässigt på spelplanen och returnerar en matris som representerar bombpositionerna.
+
+Funktionen placeSingleBomb() placeras ut en enskild bomb på en slumpmässig position på spelplanen.
+
+Funktionen cellID() genererar en unik ID-sträng för varje spelcell baserat på dess rad och kolumnposition.
+
+Funktionen createTable() skapar en HTML-tabell som representerar spelplanen och returnerar den som ett HTML-element.
+
+Funktionen addCellListeners() lägger till händelselyssnare för musklick och högerklick på en spelcell.
+
+Funktionen handleCellClick() hanterar klick på en spelcell genom att avslöja innehållet (bomb eller antal närliggande bomber).
+
+Funktionen adjacentBombs() räknar antalet närliggande bomber för en given spelcell.
+
+Funktionen adjacentFlags() räknar antalet närliggande flaggor för en given spelcell.
+
+Funktionen clickAdjacentBombs() klickar på alla närliggande celler som inte har flaggor eller redan har blivit klickade.
+
+Funktionen performMassClick() utför ett massklick på närliggande celler om den klickade cellen inte har några närliggande bomber.
+
+Funktionen gameOver() markeras spelet som förlorat genom att visa en förlorad text och en GIF-animation.
+
+Funktionen gameWon() markeras spelet som vunnet genom att visa en vinsttext.
+
+Funktionen reload() laddar om sidan för att starta om spelet.
--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-let hamburger = document.getElementById("hamburger")
-
-function show_hide() {
-    var click = document.getElementById("dropdown-content");
-    if (click.style.display === "none") {
-        click.style.display = "block";
-    } else {
-        click.style.display = "none";
-    }
-}
+let hamburger = document.getElementById("hamburger");
 
 let html = `<button class="menu-button" onclick="this.classList.toggle('opened');this.setAttribute('aria-expanded', this.classList.contains('opened'))" aria-label="Main Menu">
 <svg width="100" height="100" viewBox="0 0 100 100">
@@ -15,272 +6,402 @@ let html = `<button class="menu-button" onclick="this.classList.toggle('opened')
   <path class="line line2" d="M 20,50 H 80" />
   <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
 </svg>
-</button>`
+</button>`;
 
-hamburger.insertAdjacentHTML("beforeend", html)
+hamburger.insertAdjacentHTML("beforeend", html);
 
 // GAME
 
-var easyDiff = document.getElementsByClassName("easy");
-var mediumDiff = document.getElementsByClassName("medium");
-var hardDiff = document.getElementsByClassName("hard");
+var lostGif = document.getElementById("lostGif");
 
+var easyDiff = document.getElementById("easyDifficulty");
+var mediumDiff = document.getElementById("mediumDifficulty");
+var hardDiff = document.getElementById("hardDifficulty");
 
+var isEasy = false;
+var isMedium = true;
+var isHard = false;
+mediumDiff.style.textDecoration = "underline";
 
-// GÖR DIFFICULTY KLICKBAR OCH ÄNDRA EFTER ARRAYERNA NEDANFÖR
+function changeEasy() {
+  components = componentsEasy;
+  isEasy = true;
+  isMedium = false;
+  isHard = false;
+  easyDiff.style.textDecoration = "underline";
+  mediumDiff.style.textDecoration = "none";
+  hardDiff.style.textDecoration = "none";
+}
 
+function changeMedium() {
+  components = componentsMedium;
+  isEasy = false;
+  isMedium = true;
+  isHard = false;
+  easyDiff.style.textDecoration = "none";
+  mediumDiff.style.textDecoration = "underline";
+  hardDiff.style.textDecoration = "none";
+}
 
-
-document.getElementsByClassName("dropdown-content")[0]
-        .addEventListener("click", function (event) {
-            // do something
-        });
+function changeHard() {
+  components = componentsHard;
+  isEasy = false;
+  isMedium = false;
+  isHard = true;
+  easyDiff.style.textDecoration = "none";
+  mediumDiff.style.textDecoration = "none";
+  hardDiff.style.textDecoration = "underline";
+}
 
 var componentsEasy = {
-    num_of_rows : 6,
-    num_of_cols : 12,
-    num_of_bombs : 20,
-    bomb: String.fromCodePoint(0x1F4A3),
-    flag: String.fromCodePoint(0x1F6A9),
-    alive : true,
-    colors : {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'}
-}
+  num_of_rows: 6,
+  num_of_cols: 12,
+  num_of_bombs: 15,
+  num_of_cells: 72,
+  num_of_safe: 57,
+  bomb: String.fromCodePoint(0x1f4a3),
+  flag: String.fromCodePoint(0x1f6a9),
+  alive: true,
+  colors: {
+    1: "blue",
+    2: "green",
+    3: "red",
+    4: "purple",
+    5: "maroon",
+    6: "turquoise",
+    7: "black",
+    8: "grey",
+  },
+};
 
 var componentsMedium = {
-    num_of_rows : 9,
-    num_of_cols : 18,
-    num_of_bombs : 38,
-    bomb: String.fromCodePoint(0x1F4A3),
-    flag: String.fromCodePoint(0x1F6A9),
-    alive : true,
-    colors : {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'}
+  num_of_rows: 9,
+  num_of_cols: 18,
+  num_of_bombs: 44,
+  num_of_cells: 162,
+  num_of_safe: 134,
+  bomb: String.fromCodePoint(0x1f4a3),
+  flag: String.fromCodePoint(0x1f6a9),
+  alive: true,
+  colors: {
+    1: "blue",
+    2: "green",
+    3: "red",
+    4: "purple",
+    5: "maroon",
+    6: "turquoise",
+    7: "black",
+    8: "grey",
+  },
+};
+
+var componentsHard = {
+  num_of_rows: 12,
+  num_of_cols: 24,
+  num_of_bombs: 95,
+  num_of_cells: 288,
+  num_of_safe: 163,
+  bomb: String.fromCodePoint(0x1f4a3),
+  flag: String.fromCodePoint(0x1f6a9),
+  alive: true,
+  colors: {
+    1: "blue",
+    2: "green",
+    3: "red",
+    4: "purple",
+    5: "maroon",
+    6: "turquoise",
+    7: "black",
+    8: "grey",
+  },
+};
+
+var components = componentsMedium;
+
+function diffChecker() {
+  if (isEasy == true) {
+    startGame();
+  }
+
+  if (isMedium == true) {
+    startGame();
+  }
+
+  if (isHard == true) {
+    startGame();
+  }
 }
 
-var componentsEasy = {
-    num_of_rows : 12,
-    num_of_cols : 24,
-    num_of_bombs : 50,
-    bomb: String.fromCodePoint(0x1F4A3),
-    flag: String.fromCodePoint(0x1F6A9),
-    alive : true,
-    colors : {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'}
-}
-
-var components = {
-    num_of_rows : 12,
-    num_of_cols : 24,
-    num_of_bombs : 50,
-    bomb: String.fromCodePoint(0x1F4A3),
-    flag: String.fromCodePoint(0x1F6A9),
-    alive : true,
-    colors : {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'}
-}
-
+var firstClick = true;
+var cellsRemaining;
 function startGame() {
-    components.num_of_bombs = placeBombs();
-    document.getElementById('field').appendChild(createTable());
+  document.getElementById("startButton").style.display = "none";
+  document.getElementById("startButton").style.pointerEvents = "none";
+  document.getElementById("restartButton").style.display = "block";
+  document.getElementById("restartButton").style.pointerEvents = "all";
+  components.num_of_bombs = placeBombs();
+  cellsRemaining = components.num_of_cells;
+  document.getElementById("field").appendChild(createTable());
+  document.querySelectorAll("td").forEach(function (td) {
+    td.unopened = true;
+    td.addEventListener("mousedown", function (event) {
+      if (
+        firstClick &&
+        components.num_of_bombs[this.dataset.row][this.dataset.col]
+      ) {
+        components.num_of_bombs[this.dataset.row][this.dataset.col] = false;
+        var newBombRow, newBombCol;
+        do {
+          newBombRow = Math.floor(Math.random() * components.num_of_rows);
+          newBombCol = Math.floor(Math.random() * components.num_of_cols);
+        } while (components.num_of_bombs[newBombRow][newBombCol]);
+        components.num_of_bombs[newBombRow][newBombCol] = true;
+        if (
+          adjacentBombs(
+            parseInt(this.dataset.row),
+            parseInt(this.dataset.col)
+          ) === 0
+        ) {
+          performMassClick(
+            this,
+            parseInt(this.dataset.row),
+            parseInt(this.dataset.col)
+          );
+        }
+      }
+      firstClick = false;
+      components.mousebuttons += event.button;
+      if (event.button === 2) {
+        return;
+      }
+      if (this.flagged) {
+        return;
+      }
+      this.style.backgroundColor = "lightGrey";
+    });
+  });
 }
 
 function placeBombs() {
-    var i, rows = [];
-    
-    for (i=0; i<components.num_of_bombs; i++) {
-        placeSingleBomb(rows);
-    }
-    return rows;
-} 
+  var i,
+    rows = [];
+
+  for (i = 0; i < components.num_of_bombs; i++) {
+    placeSingleBomb(rows);
+  }
+  return rows;
+}
 
 function placeSingleBomb(bombs) {
+  var nrow, ncol, row, col;
+  nrow = Math.floor(Math.random() * components.num_of_rows);
+  ncol = Math.floor(Math.random() * components.num_of_cols);
+  row = bombs[nrow];
 
-    var nrow, ncol, row, col;
-    nrow = Math.floor(Math.random() * components.num_of_rows);
-    ncol = Math.floor(Math.random() * components.num_of_cols);
-    row = bombs[nrow];
-    
-    if (!row) {
-        row = [];
-        bombs[nrow] = row;
-    }
-    
-    col = row[ncol];
-    
-    if (!col) {
-        row[ncol] = true;
-        return
-    } 
-    else {
-        placeSingleBomb(bombs);
-    }
+  if (!row) {
+    row = [];
+    bombs[nrow] = row;
+  }
+
+  col = row[ncol];
+
+  if (!col) {
+    row[ncol] = true;
+    return;
+  } else {
+    placeSingleBomb(bombs);
+  }
 }
 
 function cellID(i, j) {
-    return 'cell-' + i + '-' + j;
+  return "cell-" + i + "-" + j;
 }
 
 function createTable() {
-    var table, row, td, i, j;
-    table = document.createElement('table');
-    
-    for (i=0; i<components.num_of_rows; i++) {
-        row = document.createElement('tr');
-        for (j=0; j<components.num_of_cols; j++) {
-            td = document.createElement('td');
-            td.id = cellID(i, j);
-            row.appendChild(td);
-            addCellListeners(td, i, j);
-        }
-        table.appendChild(row);
+  var table, row, td, i, j;
+  table = document.createElement("table");
+
+  for (i = 0; i < components.num_of_rows; i++) {
+    row = document.createElement("tr");
+    for (j = 0; j < components.num_of_cols; j++) {
+      td = document.createElement("td");
+      td.id = cellID(i, j);
+      td.dataset.row = i;
+      td.dataset.col = j;
+      row.appendChild(td);
+      addCellListeners(td, i, j);
     }
-    return table;
+    table.appendChild(row);
+  }
+  return table;
 }
 
 function addCellListeners(td, i, j) {
-    td.addEventListener('mousedown', function(event) {
-        if (!components.alive) {
-            return;
-        }
-        components.mousewhiches += event.which;
-        if (event.which === 3) {
-            return;
-        }
-        if (this.flagged) {
-            return;
-        }
-        this.style.backgroundColor = 'lightGrey';
-    });
+  td.addEventListener("mousedown", function (event) {
+    if (!components.alive) {
+      return;
+    }
+    components.mousewhiches += event.which;
+    if (event.which === 3) {
+      return;
+    }
+    if (this.flagged) {
+      return;
+    }
+    this.style.backgroundColor = "#319B54";
+  });
+  td.addEventListener("mouseup", function (event) {
+    if (!components.alive) {
+      return;
+    }
+    if (this.clicked && components.mousewhiches == 4) {
+      performMassClick(this, i, j);
+    }
+    components.mousewhiches = 0;
+    if (event.which === 3) {
+      if (this.clicked) {
+        return;
+      }
+      if (this.flagged) {
+        this.flagged = false;
+        this.textContent = "";
+      } else {
+        this.flagged = true;
+        this.textContent = components.flag;
+      }
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+    } else {
+      handleCellClick(this, i, j);
+    }
+  });
 
-    td.addEventListener('mouseup', function(event) {
-      
-        if (!components.alive) {
-            return;
-        }
-
-        if (this.clicked && components.mousewhiches == 4) {
-            performMassClick(this, i, j);
-        }
-
-        components.mousewhiches = 0;
-        
-        if (event.which === 3) {
-           
-            if (this.clicked) {
-                return;
-            }
-            if (this.flagged) {
-                this.flagged = false;
-                this.textContent = '';
-            } else {
-                this.flagged = true;
-                this.textContent = components.flag;
-            }
-
-            event.preventDefault();
-            event.stopPropagation();
-          
-            return false;
-        } 
-        else {
-            handleCellClick(this, i, j);
-        }
-    });
-
-    td.oncontextmenu = function() { 
-        return false; 
-    };
+  td.oncontextmenu = function () {
+    return false;
+  };
 }
 
 function handleCellClick(cell, i, j) {
-    if (!components.alive) {
-        return;
-    }
+  if (!components.alive) {
+    return;
+  }
 
-    if (cell.flagged) {
-        return;
-    }
+  if (cell.flagged) {
+    return;
+  }
 
-    cell.clicked = true;
+  cell.clicked = true;
 
-    if (components.num_of_bombs[i][j]) {
-        cell.style.color = 'red';
-        cell.textContent = components.bomb;
-        gameOver();
-        
+  if (components.num_of_bombs[i][j] && firstClick === false) {
+    cell.style.color = "red";
+    cell.textContent = components.bomb;
+    gameOver();
+  } else {
+    cell.style.backgroundColor = "lightGrey";
+    num_of_bombs = adjacentBombs(i, j);
+    if (num_of_bombs) {
+      cell.style.color = components.colors[num_of_bombs];
+      cell.textContent = num_of_bombs;
+    } else {
+      performMassClick(cell, i, j);
     }
-    else {
-        cell.style.backgroundColor = 'lightGrey';
-        num_of_bombs = adjacentBombs(i, j);
-        if (num_of_bombs) {
-            cell.style.color = components.colors[num_of_bombs];
-            cell.textContent = num_of_bombs;
-        } 
-        else {
-            clickAdjacentBombs(i, j);
-        }
+    if (cell.unopened) {
+      cellsRemaining -= 1;
+      cell.unopened = false;
     }
+  }
+
+  if (cellsRemaining === components.num_of_bombs) {
+    gameWon();
+  }
 }
 
 function adjacentBombs(row, col) {
-    var i, j, num_of_bombs;
-    num_of_bombs = 0;
+  var i, j, num_of_bombs;
+  num_of_bombs = 0;
 
-    for (i=-1; i<2; i++) {
-        for (j=-1; j<2; j++) {
-            if (components.num_of_bombs[row + i] && components.num_of_bombs[row + i][col + j]) {
-                num_of_bombs++;
-            }
-        }
+  for (i = -1; i < 2; i++) {
+    for (j = -1; j < 2; j++) {
+      if (
+        components.num_of_bombs[row + i] &&
+        components.num_of_bombs[row + i][col + j]
+      ) {
+        num_of_bombs++;
+      }
     }
-    return num_of_bombs;
+  }
+  return num_of_bombs;
 }
 
 function adjacentFlags(row, col) {
-    var i, j, num_flags;
-    num_flags = 0;
+  var i, j, num_flags;
+  num_flags = 0;
 
-    for (i=-1; i<2; i++) {
-        for (j=-1; j<2; j++) {
-            cell = document.getElementById(cellID(row + i, col + j));
-            if (!!cell && cell.flagged) {
-                num_flags++;
-            }
-        }
+  for (i = -1; i < 2; i++) {
+    for (j = -1; j < 2; j++) {
+      cell = document.getElementById(cellID(row + i, col + j));
+      if (!!cell && cell.flagged) {
+        num_flags++;
+      }
     }
-    return num_flags;
+  }
+  return num_flags;
 }
 
 function clickAdjacentBombs(row, col) {
-    var i, j, cell;
-    
-    for (i=-1; i<2; i++) {
-        for (j=-1; j<2; j++) {
-            if (i === 0 && j === 0) {
-                continue;
-            }
-            cell = document.getElementById(cellID(row + i, col + j));
-            if (!!cell && !cell.clicked && !cell.flagged) {
-                handleCellClick(cell, row + i, col + j);
-            }
-        }
+  var i, j, cell;
+
+  for (i = -1; i < 2; i++) {
+    for (j = -1; j < 2; j++) {
+      if (i === 0 && j === 0) {
+        continue;
+      }
+      cell = document.getElementById(cellID(row + i, col + j));
+
+      if (!!cell && !cell.clicked && !cell.flagged) {
+        handleCellClick(cell, row + i, col + j);
+      }
     }
+  }
 }
 
 function performMassClick(cell, row, col) {
-    if (adjacentFlags(row, col) === adjacentBombs(row, col)) {
-        clickAdjacentBombs(row, col);
+  var i, j;
+  for (i = -1; i < 2; i++) {
+    for (j = -1; j < 2; j++) {
+      var adjacentCell = document.getElementById(cellID(row + i, col + j));
+      if (adjacentCell && !adjacentCell.clicked && !adjacentCell.flagged) {
+        handleCellClick(adjacentCell, row + i, col + j);
+      }
     }
+  }
 }
+
+var clientX;
+var clientY;
+var gifWidth = lostGif.clientWidth / 2;
+var gifHeight = lostGif.clientHeight / 2;
+document.onclick = (event) => {
+  deathCenterX = event.clientX - gifWidth;
+  deathCenterY = event.clientY - gifHeight;
+};
 
 function gameOver() {
-    components.alive = false;
-    document.getElementById('lost').style.display="block";
-    
+  components.alive = false;
+  document.getElementById("lostText").style.display = "block";
+  lostGif.style.display = "block";
+  lostGif.style.position = "absolute";
+  lostGif.style.left = deathCenterX + "px";
+  lostGif.style.top = deathCenterY + "px";
+  lostGif.style.zIndex = "10";
+  lostGif.style.pointerEvents = "none";
 }
 
-function reload(){
-    window.location.reload();
+function gameWon() {
+  document.getElementById("wonText").style.display = "block";
 }
 
-window.addEventListener('load', function() {
-    document.getElementById('lost').style.display="none";
-    startGame();
-});
+function reload() {
+  window.location.reload();
+}
